b6080082d0a3b3395ba4f24b9586264fc8ef2082
Lorenzo Tacconi <lorenzo.tacconi6@gmail.com>
2015-10-30 10:18:39 +0100

    Aggiunto Metodo Encode in Vigenere32

--- CryptoAlpha/AlphaCypher/Vigenere32.cs
+++ CryptoAlpha/AlphaCypher/Vigenere32.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WallF.BaseNEncodings;
 
 namespace AlphaCypher
 {
-    class Vigenere32
+    public class Vigenere32 : Vigenere
     {
+        public override string Encode(string text, string cypher)
+        {
+            string resp = "";
+            string tmp = base.Encode(text, cypher);
+            byte[] vettToEncode = Encoding.UTF8.GetBytes(tmp);
+            char[] Encoded = Base32Encoding.Base32.Encode(vettToEncode);
+            resp = new string(Encoded);
+            return resp;
+        }
+        public override string Decode(string text, string cypher)
+        {
+            return "";
+        }
+        
     }
 }
